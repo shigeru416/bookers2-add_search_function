7d97d2b065b73da5f075b9f2d0e02ac57ac74987
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-05 12:14:15 +0900

    [start]5

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  
+  root 'home#top'
+  get 'home/about'
+  devise_for :users
+  resources :users
+  get 'users/:id/follows'=> 'users#follows', as: 'follows'
+  get 'users/:id/followers'=> 'users#followers', as: 'followers'
+  
+  resources :books do
+   resource :book_comments, only:[:create, :destroy]
+   resource :favorites, only: [:create, :destroy]
+  end
+
+  post 'follow/:id' => 'relationships#create', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow' # フォロー外す
+  
+end
